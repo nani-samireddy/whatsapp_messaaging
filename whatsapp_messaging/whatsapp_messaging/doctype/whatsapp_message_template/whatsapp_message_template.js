af1d797f84b38677411f89a9f7f9a1d992f67341
nani-samireddy <nanisamireddy05@gmail.com>
2024-07-23 21:39:01 +0530

    feat(template): added phone number field selection for doc type

--- whatsapp_messaging/whatsapp_messaging/doctype/whatsapp_message_template/whatsapp_message_template.js
+++ whatsapp_messaging/whatsapp_messaging/doctype/whatsapp_message_template/whatsapp_message_template.js
@@ -3,158 +3,193 @@
 
 // Custom script for the main DocType
 frappe.ui.form.on("WhatsApp Message Template", {
-  text_template_text_message: function (frm) {
-    let message = frm.doc.text_template_text_message;
-    let regex = /\{\{(\d+)\}\}/g;
-    let matches = [];
-    let match;
+	text_template_text_message: function (frm) {
+		let message = frm.doc.text_template_text_message;
+		let regex = /\{\{(\d+)\}\}/g;
+		let matches = [];
+		let match;
 
-    // Find all matches
-    while ((match = regex.exec(message)) !== null) {
-      matches.push(parseInt(match[1], 10));
-    }
+		// Find all matches
+		while ((match = regex.exec(message)) !== null) {
+			matches.push(parseInt(match[1], 10));
+		}
 
-    // Add unique matches to the child table
-    matches = [...new Set(matches)]; // Remove duplicates
-    matches.forEach((position) => {
-      let exists = frm.doc.text_template_fields.some(
-        (field) => field.field_position === position.toString(),
-      );
-      if (!exists) {
-        let new_row = frm.add_child("text_template_fields");
-        frappe.model.set_value(
-          new_row.doctype,
-          new_row.name,
-          "field_position",
-          position.toString(),
-        );
-      }
-    });
+		// Add unique matches to the child table
+		matches = [...new Set(matches)]; // Remove duplicates
+		matches.forEach((position) => {
+			let exists = frm.doc.text_template_fields.some(
+				(field) => field.field_position === position.toString(),
+			);
 
-    // Refresh the child table field
-    frm.refresh_field("text_template_fields");
-  },
+			if (!exists) {
+				let new_row = frm.add_child("text_template_fields");
 
-  refresh: function (frm) {
-    // Attach the change event to the child table.
-    frm.fields_dict["text_template_fields"].grid.wrapper.on(
-      "change",
-      'input[data-fieldname="field_doc_type"]',
-      function () {
-        update_template_field_option_names(frm);
-      },
-    );
-  },
+				// Set the field position
+				frappe.model.set_value(
+					new_row.doctype,
+					new_row.name,
+					"field_position",
+					position.toString(),
+				);
 
-  // Trigger when the form is loaded or the child table is refreshed
-  onload_post_render: function (frm) {
-    frm.fields_dict["text_template_fields"].grid.wrapper.on(
-      "change",
-      'input[data-fieldname="field_doc_type"]',
-      function () {
-        update_template_field_option_names(frm);
-      },
-    );
-  },
+				// Set default field type.
+				frappe.model.set_value(
+					new_row.doctype,
+					new_row.name,
+					"field_type",
+					"This Doc",
+				);
+
+				// Set default doc type.
+				frappe.model.set_value(
+					new_row.doctype,
+					new_row.name,
+					"field_doc_type",
+					frm.doc.template_doctype,
+				);
+
+				// Set the options for the field name.
+				update_template_field_option_names(frm);
+			}
+		});
+
+		// Refresh the child table field
+		frm.refresh_field("text_template_fields");
+	},
+
+	template_doctype: function (frm) {
+		// Update the field doc type for all the child table rows if field type is "This Doc".
+		frm.doc.text_template_fields.forEach((row, index) => {
+			if (row.field_type === "This Doc") {
+				frappe.model.set_value(
+					row.doctype,
+					row.name,
+					"field_doc_type",
+					frm.doc.template_doctype,
+				);
+			}
+		});
+
+		// Add phone number fields of template_doctype to phone_number_field_name as options
+		update_phone_number_field_options(frm, frm.doc.template_doctype, ["Phone"]);
+	},
+
+	refresh: function (frm) {
+		// Attach the change event to the child table.
+		frm.fields_dict["text_template_fields"].grid.wrapper.on(
+			"change",
+			'input[data-fieldname="field_doc_type"]',
+			function () {
+				update_template_field_option_names(frm);
+			},
+		);
+
+		// Add phone number fields of template_doctype to phone_number_field_name as options
+		update_phone_number_field_options(frm, frm.doc.template_doctype, ["Phone"]);
+	},
+
+	// Trigger when the form is loaded or the child table is refreshed.
+	onload_post_render: function (frm) {
+		frm.fields_dict["text_template_fields"].grid.wrapper.on(
+			"change",
+			'input[data-fieldname="field_doc_type"]',
+			function () {
+				update_template_field_option_names(frm);
+			},
+		);
+
+	},
 });
 
-function update_template_field_option_names(frm) {
-  frm.doc.text_template_fields.forEach((row, index) => {
-    let field_doc_type = row.field_doc_type;
-    if (field_doc_type) {
-      frappe.model.with_doctype(field_doc_type, () => {
-        let fields = frappe.get_meta(field_doc_type).fields;
+function update_phone_number_field_options(frm, doctype, input_fieldtypes = []) {
 
-        // Filter only input fields.
-        let input_fieldtypes = [
-          "Data",
-          "Select",
-          "Date",
-          "Datetime",
-          "Time",
-          "Currency",
-          "Int",
-          "Float",
-          "Check",
-          "Text",
-          "Small Text",
-          "Long Text",
-          "Link",
-          "Dynamic Link",
-          "Password",
-          "Read Only",
-          "Attach",
-          "Attach Image",
-        ];
+	// Add default input field types.
+	if (input_fieldtypes.length === 0) {
+		input_fieldtypes = [
+			"Data",
+			"Select",
+			"Date",
+			"Datetime",
+			"Time",
+			"Currency",
+			"Int",
+			"Float",
+			"Check",
+			"Text",
+			"Small Text",
+			"Long Text",
+			"Link",
+			"Dynamic Link",
+			"Password",
+			"Phone",
+			"Read Only",
+			"Attach",
+			"Attach Image",
+		];
+	}
+	let options = [];
+	frappe.model.with_doctype(doctype, () => {
+		let fields = frappe.get_meta(doctype).fields;
 
-        // Use fieldname as value.
-        let options = fields
-          .filter((field) => input_fieldtypes.includes(field.fieldtype))
-          .map((field) => field.fieldname);
-        console.log(options);
-        console.log(frm.doc.name);
+		// Filter only input fields.
+		let input_fields = fields.filter((field) =>
+			input_fieldtypes.includes(field.fieldtype),
+		);
 
-        // Update field options for the child table.
-        frm.fields_dict["text_template_fields"].grid.update_docfield_property(
-          "field_name",
-          "options",
-          [""].concat(options),
-        );
+		// Use fieldname as value.
+		options = input_fields.map((field) => field.fieldname);
 
-        // Refresh the specific field in the child table.
-        frm.fields_dict["text_template_fields"].grid.refresh();
-      });
-    }
-  });
+		frm.fields_dict["phone_number_field_name"].df.options = options;
+		frm.fields_dict["phone_number_field_name"].refresh();
+	});
+
+	return options;
 }
 
-// // Custom script for the child DocType
-// frappe.ui.form.on("Whatsapp Message Template Field", {
-//   field_doc_type: function (frm, cdt, cdn) {
-//     let child = locals[cdt][cdn];
-//     if (child.field_doc_type) {
-//       frappe.model.with_doctype(child.field_doc_type, () => {
-//         let fields = frappe.get_meta(child.field_doc_type).fields;
+function update_template_field_option_names(frm) {
+	frm.doc.text_template_fields.forEach((row, index) => {
+		let field_doc_type = row.field_doc_type;
+		if (field_doc_type) {
+			frappe.model.with_doctype(field_doc_type, () => {
+				let fields = frappe.get_meta(field_doc_type).fields;
 
-//         // Filter only input fields
-//         let input_fieldtypes = [
-//           "Data",
-//           "Select",
-//           "Date",
-//           "Datetime",
-//           "Time",
-//           "Currency",
-//           "Int",
-//           "Float",
-//           "Check",
-//           "Text",
-//           "Small Text",
-//           "Long Text",
-//           "Link",
-//           "Dynamic Link",
-//           "Password",
-//           "Read Only",
-//           "Attach",
-//           "Attach Image",
-//         ];
+				// Filter only input fields.
+				let input_fieldtypes = [
+					"Data",
+					"Select",
+					"Date",
+					"Datetime",
+					"Time",
+					"Currency",
+					"Int",
+					"Float",
+					"Check",
+					"Text",
+					"Small Text",
+					"Long Text",
+					"Link",
+					"Dynamic Link",
+					"Password",
+					"Read Only",
+					"Attach",
+					"Attach Image",
+				];
 
-//         let options = fields
-//           .filter((field) => input_fieldtypes.includes(field.fieldtype))
-//           .map((field) => field.fieldname); // Use fieldname as value
+				// Use fieldname as value.
+				let options = fields
+					.filter((field) => input_fieldtypes.includes(field.fieldtype))
+					.map((field) => field.fieldname);
 
-//         console.log(options);
-//         // Update field options for the child table
-//         frappe.meta.get_docfield(
-//           "Whatsapp Message Template Field",
-//           "field_name",
-//           frm.doc.name,
-//         ).options = options.join("\n");
+				// Update field options for the child table.
+				frm.fields_dict["text_template_fields"].grid.update_docfield_property(
+					"field_name",
+					"options",
+					[""].concat(options),
+				);
 
-//         // Refresh the specific field in the child table
-//         frm.fields_dict["text_template_fields"].grid.grid_rows_by_docname[
-//           cdn
-//         ].refresh_field("field_name");
-//       });
-//     }
-//   },
-// });
+				// Refresh the specific field in the child table.
+				frm.fields_dict["text_template_fields"].grid.refresh();
+			});
+		}
+	});
+}
